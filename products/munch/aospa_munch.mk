a0e478c0b49ff5e7bcec8dd91d29017869629224
olzhas0986 <kdiroljas@gmail.com>
2026-08-10 14:43:22 +0500

    products: munch: Override build desc and fingerprint

--- products/munch/aospa_munch.mk
+++ products/munch/aospa_munch.mk
@@ -26,4 +26,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Boot animation resolution.
 TARGET_BOOT_ANIMATION_RES := 1080
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.12.0.ULMMIXM release-keys" \
+    BuildFingerprint=Redmi/munch/munch:13/RKQ1.211001.001/V816.0.12.0.ULMMIXM:user/release-keys
+
 endif
